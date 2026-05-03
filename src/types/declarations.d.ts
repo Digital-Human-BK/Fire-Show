5274331237f597bd2720675ca72841668917dc2c
Digital Human <85568905+Digital-Human-BK@users.noreply.github.com>
2026-05-04 00:17:22 +0300

    feat: integrate Contentful for site content management

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -4,3 +4,12 @@ declare module "@fontsource-variable/space-grotesk";
 declare module "@fontsource-variable/manrope";
 declare module "swiper/css";
 declare module "swiper/css/*";
+
+interface ImportMetaEnv {
+  readonly VITE_CONTENTFUL_SPACE_ID: string;
+  readonly VITE_CONTENTFUL_ACCESS_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
